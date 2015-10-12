48dca1ab9443c1b491dce18db41c4ca993ed5609
byuan98006 <byuan@tablesafe.com>
2015-10-12 12:25:52 -0700

    add submit button handler for login

--- nodePortals/controllers/account_controller.js
+++ nodePortals/controllers/account_controller.js
@@ -6,4 +6,18 @@ router.route('/')
     res.render('./account/login');
 });
 
+router.route('/login')
+    .post(function (req, res, next) {
+    var username = req.body.username;
+    var password = req.body.password;
+    if ((username == null) && (password == null)) {
+        return res.render('./account/login');
+    }
+    
+    if ((username == null) || (password == null)) {
+        return res.render('./account/login', {status: 'Please specify a username and password.'})
+    }
+    console.log('Username: ' + req.body.username);
+});
+
 module.exports = router;
